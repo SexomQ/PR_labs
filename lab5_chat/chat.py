7e617ac3883c2faba0399a9c2713b7e4dc93f1da
Sexomq <sclifos-tudor@mail.ru>
2023-10-11 17:18:38 +0300

    add concept of rooms

--- lab5_chat/chat.py
+++ lab5_chat/chat.py
@@ -1,5 +1,7 @@
 import socket
 import threading
+import json
+import os
 
 # Server configuration
 HOST = '127.0.0.1' # Loopback address for localhost
@@ -17,62 +19,102 @@ server_socket.listen(5)
 print(f"Server is listening on {HOST}:{PORT}")
 
 
-
-
-
-
-
-
-
 # Function to handle a client's messages
 def handle_client(client_socket, client_address):
-    accepted_dict = {     
-        "type": "connect_ack",
-        "payload": {
-            "message": f"Connected to the room: {client_address}."
-        }
-    }
 
-    print(f"Accepted connection : {accepted_dict}")
-
-    final_mess = accepted_dict
-    for client in clients:
-            # if client != client_socket:
-            client.send(final_mess.encode('utf-8'))
-
-    print(f"Accepted connection : {accepted_dict}")
+    print(f"Accepted connection to server: {client_address}")
 
     while True:
-        message = client_socket.recv(1024).decode('utf-8').to_dict()
+        message = client_socket.recv(1024).decode('utf-8')
+        message = json.loads(message)
+
         if not message:
             break  # Exit the loop when the client disconnects
 
         print(f"Received from {client_address}: {message}")
 
-        if message["type"] == "message":
-            final_mess = message
-            
         if message["type"] == "connect":
-            final_mess = {
-                "type": "notification",
+            room = message["payload"]["room"]
+            name = message["payload"]["name"]
+            if room not in rooms_clients:
+                rooms_clients[room] = []
+                
+                folder = room + "-" + "Folder"
+                
+                if not os.path.exists(folder):
+                    rooms_folsers[room] = folder
+                    os.mkdir(folder)
+            rooms_clients[room].append([client_socket, name])
+            rooms_folsers[room].append([folder])
+            for client in rooms_clients[room]:
+                if client[0] == client_socket:
+                    return_message = {
+                        "type": "connect_ack",
+                        "payload": {
+                            "message": f"Connected to the room: {message['payload']['room']}."
+                        }
+                    }
+                    client[0].send(json.dumps(return_message).encode('utf-8'))
+                else:
+                    return_message = {
+                        "type": "notification",
+                        "payload": {
+                            "message": f"{message['payload']['name']} has joined the room."
+                        }
+                    }
+                    client[0].send(json.dumps(return_message).encode('utf-8'))
+
+
+        if message["type"] == "message" and message["payload"]["room"] != "":
+            room = message["payload"]["room"]
+            name = message["payload"]["sender"]
+            text = message["payload"]["text"]
+
+            return_message = {
+                "type": "message",
                 "payload": {
-                    "message": f"{message['payload']['message']} has joined the room."
+                    "sender": name,
+                    "room": room,
+                    "text": text
                 }
             }
 
-        
+            # Broadcast the message to all clients
+            for client in rooms_clients[room]:
+                if client[0] != client_socket:
+                    client[0].send(json.dumps(return_message).encode('utf-8'))
 
-        # Broadcast the message to all clients
-        for client in clients:
-            # if client != client_socket:
-            client.send(final_mess.encode('utf-8'))
+        if message["type"] == "upload":
+            room = message["payload"]["room"]
+            name = message["payload"]["sender"]
+            file_name = message["payload"]["file_name"]
+            file_data = message["payload"]["file"]
 
+            return_message = {
+                "type": "notification_upload",
+                "payload": {
+                    "sender": name,
+                    "room": room,
+                    "file_name": file_name,
+                    "file": file_data
+                }
+            }
+
+            # Broadcast the message to all clients
+            for client in rooms_clients[room]:
+                if client[0] != client_socket:
+                    client[0].send(json.dumps(return_message).encode('utf-8'))
+
+            
     # Remove the client from the list
     clients.remove(client_socket)
     client_socket.close()
 
 
+# dictionary of rooms and their clients
 clients = []
+rooms_clients = {}
+rooms_folsers = {}
 
 
 
--- lab5_chat/client.py
+++ lab5_chat/client.py
@@ -1,6 +1,7 @@
 import json
 import socket
 import threading
+import os 
 
 # server config
 HOST = "127.0.0.1"  # Localhost
@@ -22,14 +23,18 @@ print(f"[*] Listening as {HOST}:{PORT}")
 def receive_messages():
     while True:
         message = client_socket.recv(1024).decode('utf-8')
-
+        message = json.loads(message)
         if not message:
             break  # Exit the loop when the server disconnects
         
         if message["type"] == "connect_ack":
-            print(f"Connected to the room: {message['payload']['message']}")
+            print(f"{message['payload']['message']}")
         
-        print(f"{message['payload']['sender']}: {message['payload']['text']}")
+        if message["type"] == "notification":
+            print(f"{message['payload']['message']}")
+        
+        if message["type"] == "message":
+            print(f"{message['payload']['sender']}: {message['payload']['text']}")
 
 
 # Start the message reception thread
@@ -37,8 +42,10 @@ receive_thread = threading.Thread(target=receive_messages)
 receive_thread.daemon = True  # Thread will exit when the main program exits
 receive_thread.start()
 
+name = ""
+room = ""
 while True:
-    message = input("Enter a message: ")
+    message = input()
 
     if message.lower() == 'exit':
         break
@@ -56,17 +63,40 @@ while True:
          }
         client_socket.send(json.dumps(message_json).encode('utf-8'))
 
-    mess = {
-        "type": "message",
-        "payload": {
-            "sender": name,
-            "room": room,
-            "text": message
-        }
-    }
+    if message.lower() == "upload":
+        file_name = input("Enter file path: ")
+        path = os.path.basename(file_name)
 
-    # Send the message to the server
-    client_socket.send(json.dumps(mess).encode('utf-8'))
+        file = open(file_name, "rb")
+
+        file_data = file.read(1024)
+        while file_data:
+            message_json = {
+                "type": "upload",
+                "payload": {
+                    "sender": name,
+                    "room": room,
+                    "file_name": os.path.split(path)[1],
+                    "file": file_data
+                }
+            }
+
+            client_socket.send(json.dumps(message_json).encode('utf-8'))
+            file_data = file.read(1024)
+        file.close()
+
+    else:
+        mess = {
+            "type": "message",
+            "payload": {
+                "sender": name,
+                "room": room,
+                "text": message
+            }
+        }
+
+        # Send the message to the server
+        client_socket.send(json.dumps(mess).encode('utf-8'))
 
 # Close the client socket when done
 client_socket.close()
