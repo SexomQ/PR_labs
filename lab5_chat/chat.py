05fd8e97f13eda932fe3b0823fb3eaf3a24406b1
Sexomq <sclifos-tudor@mail.ru>
2023-10-12 14:55:01 +0300

    lab 5 for Vasika

--- lab5_chat/chat.py
+++ lab5_chat/chat.py
@@ -2,6 +2,7 @@ import socket
 import threading
 import json
 import os
+import base64
 
 # Server configuration
 HOST = '127.0.0.1' # Loopback address for localhost
@@ -18,6 +19,20 @@ server_socket.bind((HOST, PORT))
 server_socket.listen(5)
 print(f"Server is listening on {HOST}:{PORT}")
 
+def create_connection_for_binary_transfer(client_socket, room, file_name):
+    try:
+        file_size = int(client_socket.recv(10).decode("utf-8").strip())
+        received_size = 0
+        with open(rooms_folsers.get(room) + "/" + file_name, "wb") as file:
+            while received_size < file_size:
+                file_data = client_socket.recv(1024)
+                if not file_data:
+                    break
+                file.write(file_data)
+                received_size += len(file_data)
+        print("File has been received successfully.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 # Function to handle a client's messages
 def handle_client(client_socket, client_address):
@@ -25,8 +40,11 @@ def handle_client(client_socket, client_address):
     print(f"Accepted connection to server: {client_address}")
 
     while True:
-        message = client_socket.recv(1024).decode('utf-8')
-        message = json.loads(message)
+        got_message = client_socket.recv(1024)
+
+    
+        user_message = got_message.decode('utf-8')
+        message = json.loads(user_message)
 
         if not message:
             break  # Exit the loop when the client disconnects
@@ -34,18 +52,22 @@ def handle_client(client_socket, client_address):
         print(f"Received from {client_address}: {message}")
 
         if message["type"] == "connect":
+            request_type = "json"
             room = message["payload"]["room"]
             name = message["payload"]["name"]
             if room not in rooms_clients:
                 rooms_clients[room] = []
                 
-                folder = room + "-" + "Folder"
-                
-                if not os.path.exists(folder):
-                    rooms_folsers[room] = folder
-                    os.mkdir(folder)
+            folder = room + "-" + "Folder"
+            
+            if not os.path.exists(folder):
+                rooms_folsers[room] = folder
+                os.mkdir(folder)
+            
+            elif folder not in rooms_folsers:
+                rooms_folsers[room] = folder
+            
             rooms_clients[room].append([client_socket, name])
-            rooms_folsers[room].append([folder])
             for client in rooms_clients[room]:
                 if client[0] == client_socket:
                     return_message = {
@@ -65,7 +87,8 @@ def handle_client(client_socket, client_address):
                     client[0].send(json.dumps(return_message).encode('utf-8'))
 
 
-        if message["type"] == "message" and message["payload"]["room"] != "":
+        elif message["type"] == "message" and message["payload"]["room"] != "":
+            request_type = "json"
             room = message["payload"]["room"]
             name = message["payload"]["sender"]
             text = message["payload"]["text"]
@@ -84,28 +107,115 @@ def handle_client(client_socket, client_address):
                 if client[0] != client_socket:
                     client[0].send(json.dumps(return_message).encode('utf-8'))
 
-        if message["type"] == "upload":
+        elif message["type"] == "upload":
+            if message["payload"]["file_type"] == "txt":
+                request_type = "json"
+                room = message["payload"]["room"]
+                name = message["payload"]["sender"]
+                file_name = message["payload"]["file_name"]
+                file_data = message["payload"]["file"]
+
+                if file_data == "EOF":
+                    return_message = {
+                        "type": "notification_upload",
+                        "payload": {
+                            "sender": name,
+                            "room": room,
+                            "file_name": file_name,
+                            "file": file_data,
+                            "message": f"{message['payload']['sender']} has uploaded a file: {message['payload']['file_name']}.",
+                            "file_type": "txt"
+                        }
+                    }
+
+                    # Broadcast the message to all clients
+                    for client in rooms_clients[room]:
+                        if client[0] != client_socket:
+                            client[0].send(json.dumps(return_message).encode('utf-8'))
+                else:
+                    file_data = base64.b64decode(message["payload"]["file"] + "=" * ((4 - len(message["payload"]["file"]) % 4) % 4))
+                    
+                    print(rooms_folsers.get(room))
+                    uploaded_file = open(rooms_folsers.get(room) + "/" + file_name, "wb") 
+                    uploaded_file.write(file_data)
+                    uploaded_file.close()
+
+            elif message["payload"]["file_type"] == "png":
+                room = message["payload"]["room"]
+                name = message["payload"]["sender"]
+                file_name = message["payload"]["file_name"]
+                file_data = message["payload"]["file"]
+                    
+                if file_data == "":
+                    create_connection_for_binary_transfer(client_socket, room, file_name)
+                    
+                    return_message = {
+                        "type": "notification_upload",
+                        "payload": {
+                            "sender": name,
+                            "room": room,
+                            "file_name": file_name,
+                            "file": file_data,
+                            "message": f"{message['payload']['sender']} has uploaded a file: {message['payload']['file_name']}.",
+                            "file_type": "png"
+                        }
+                    }
+
+                    # Broadcast the message to all clients
+                    for client in rooms_clients[room]:
+                        if client[0] != client_socket:
+                            client[0].send(json.dumps(return_message).encode('utf-8'))
+
+
+        elif message["type"] == "download":
+            
             room = message["payload"]["room"]
             name = message["payload"]["sender"]
             file_name = message["payload"]["file_name"]
-            file_data = message["payload"]["file"]
 
-            return_message = {
-                "type": "notification_upload",
-                "payload": {
-                    "sender": name,
-                    "room": room,
-                    "file_name": file_name,
-                    "file": file_data
-                }
-            }
+            if os.path.exists(rooms_folsers.get(room) + "/" + file_name):
+                file = open(rooms_folsers.get(room) + "/" + file_name, "rb")
 
-            # Broadcast the message to all clients
-            for client in rooms_clients[room]:
-                if client[0] != client_socket:
-                    client[0].send(json.dumps(return_message).encode('utf-8'))
+                file_data = file.read(1024)
+                while file_data:
+                    return_message = {
+                        "type": "request_download",
+                        "payload": {
+                            "sender": name,
+                            "room": room,
+                            "file_name": file_name,
+                            "file": str(base64.b64encode(file_data))[2:-1]
+                        }
+                    }
 
+                    client_socket.send(json.dumps(return_message).encode('utf-8'))
+                    file_data = file.read(1024)
+
+                return_message = {
+                        "type": "request_download",
+                        "payload": {
+                            "sender": name,
+                            "room": room,
+                            "file_name": file_name,
+                            "file": "EOF"
+                        }
+                    }
+                client_socket.send(json.dumps(return_message).encode('utf-8'))
+                file.close()
             
+            else:
+                return_message = {
+                        "type": "request_download",
+                        "payload": {
+                            "sender": name,
+                            "room": room,
+                            "file_name": file_name,
+                            "file": "#"
+                        }
+                    }
+                client_socket.send(json.dumps(return_message).encode('utf-8'))
+
+        
     # Remove the client from the list
     clients.remove(client_socket)
     client_socket.close()
@@ -115,7 +225,7 @@ def handle_client(client_socket, client_address):
 clients = []
 rooms_clients = {}
 rooms_folsers = {}
-
+request_type = "json"
 
 
 
--- lab5_chat/client.py
+++ lab5_chat/client.py
@@ -2,6 +2,7 @@ import json
 import socket
 import threading
 import os 
+import base64
 
 # server config
 HOST = "127.0.0.1"  # Localhost
@@ -22,8 +23,8 @@ print(f"[*] Listening as {HOST}:{PORT}")
 
 def receive_messages():
     while True:
-        message = client_socket.recv(1024).decode('utf-8')
-        message = json.loads(message)
+        mess = client_socket.recv(1024).decode('utf-8')
+        message = json.loads(mess)
         if not message:
             break  # Exit the loop when the server disconnects
         
@@ -35,6 +36,20 @@ def receive_messages():
         
         if message["type"] == "message":
             print(f"{message['payload']['sender']}: {message['payload']['text']}")
+        
+        if message["type"] == "notification_upload":
+            print(f"{message['payload']['message']}")
+
+        if message["type"] == "request_download":
+            if message["payload"]["file"] == "#":
+                print(f"File {message['payload']['file_name']} does not exist")
+            elif message["payload"]["file"] == "EOF":
+                print(f"{message['payload']['file_name']} downloaded")
+            else:
+                file_name = message["payload"]["file_name"]
+                file = open(file_name, "wb")
+                file.write(base64.b64decode(message["payload"]["file"] + "=" * ((4 - len(message["payload"]["file"]) % 4) % 4)))
+                file.close()
 
 
 # Start the message reception thread
@@ -50,7 +65,7 @@ while True:
     if message.lower() == 'exit':
         break
 
-    if message.lower() == 'connect':
+    elif message.lower() == 'connect':
         room = input("enter room: ")
         name = input("enter name: ")
 
@@ -63,27 +78,91 @@ while True:
          }
         client_socket.send(json.dumps(message_json).encode('utf-8'))
 
-    if message.lower() == "upload":
-        file_name = input("Enter file path: ")
-        path = os.path.basename(file_name)
+    elif message.lower().split(":")[0] == "upload":
+        if message.split(".")[1] == "txt":
+            file_name = message.split(" ")[1]
+            if os.path.exists(file_name):
+                path = os.path.basename(file_name)
 
-        file = open(file_name, "rb")
+                file = open(file_name, "rb")
 
-        file_data = file.read(1024)
-        while file_data:
-            message_json = {
-                "type": "upload",
-                "payload": {
-                    "sender": name,
-                    "room": room,
-                    "file_name": os.path.split(path)[1],
-                    "file": file_data
+                file_data = file.read(1024)
+                while file_data:
+                    message_json = {
+                        "type": "upload",
+                        "payload": {
+                            "sender": name,
+                            "room": room,
+                            "file_name": os.path.split(path)[1],
+                            "file": str(base64.b64encode(file_data))[2:-1],
+                            "file_type": "txt"
+                        }
+                    }
+
+                    client_socket.send(json.dumps(message_json).encode('utf-8'))
+                    file_data = file.read(1024)
+                message_json = {
+                        "type": "upload",
+                        "payload": {
+                            "sender": name,
+                            "room": room,
+                            "file_name": os.path.split(path)[1],
+                            "file": "EOF",
+                            "file_type": "txt"
+                        }
+                    }
+                client_socket.send(json.dumps(message_json).encode('utf-8'))
+                file.close()
+            else:
+                print(f"File {file_name} does not exist")
+        elif message.split(".")[1] == "png":
+            file_name = message.split(" ")[1]
+            if os.path.exists(file_name):
+                path = os.path.basename(file_name)
+
+                # request to change the server to work with bin
+                message_json = {
+                        "type": "upload",
+                        "payload": {
+                            "sender": name,
+                            "room": room,
+                            "file_name": os.path.split(path)[1],
+                            "file": "",
+                            "file_type": "png"
+                        }
+                    }
+                
+                client_socket.send(json.dumps(message_json).encode('utf-8'))
+
+                try:
+                    file = open(file_name, 'rb')
+                    file_size = os.path.getsize(file_name)
+                    client_socket.send(f"{file_size:<10}".encode('utf-8'))
+                    
+                    while True:
+                        file_data = file.read(1024)
+                        if not file_data:
+                            break
+                        client_socket.send(file_data)
+                except Exception as e:
+                    print(f"An error occurred: {e}")
+                finally:
+                    file.close()
+
+            else:
+                print(f"File {file_name} does not exist")
+
+    elif message.lower().split(":")[0] == "download":
+        file_name = message.split(" ")[1]
+        message_json = {
+                    "type": "download",
+                    "payload": {
+                        "sender": name,
+                        "room": room,
+                        "file_name": file_name
+                    }
                 }
-            }
-
-            client_socket.send(json.dumps(message_json).encode('utf-8'))
-            file_data = file.read(1024)
-        file.close()
+        client_socket.send(json.dumps(message_json).encode('utf-8'))
 
     else:
         mess = {
